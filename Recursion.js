2df9976fe309ae685e66fec37aafc5c00841991e
Ubuntu <ubuntu@ip-172-31-79-40.ec2.internal>
2021-01-17 19:49:17 +0000

    Initial Commit

---
+++ Recursion.js
@@ -0,0 +1,27 @@
+/*
+* Copyright (c) 2021
+*
+* The Recursion file reverses a word and prints it
+* out to the user.
+* 
+* @author  Wajd Mariam
+* @version 1.0
+* @since   2021/01/04
+*/
+
+function reverseString(string) {
+  if (string.length == 0) {
+    return string;
+  } else {
+    return reverseString(string.substring(1)) + string.charAt(0);
+  }
+}
+
+// Assigning string.
+let recursionWord = "recursion";
+// Printing the original string.
+console.log(`The original string is ${recursionWord}`);
+// Calling reverseString function to reverse recursionWord.
+let newWord = reverseString(recursionWord);
+// Printing the new reversed string.
+console.log(`The reversed string (using recursion) is: ${newWord}`);
